f917c54e88e2c5cd96f291a4f76b33b43a109e4a
Umut <sumutyigitoglu@hotmail.com>
2022-04-12 03:45:20 +0300

    fixed json and other stuff

--- dweller/dweller.ino
+++ dweller/dweller.ino
@@ -13,78 +13,117 @@
 #define GREEN_RGB 3
 #define BLUE_RGB 5
 #define BUZZER 4
-
 #define LID A0
 #define LDR0 A1
 #define LDR1 A2
 #define LDR2 A3
-#define LDR_SENSITIVITY 990
+#define LDR_SENSITIVITY 600
 #define RST_PIN  9
 #define SS_PIN 10
 #define CE 7
 #define CSN 8
 
-int ADXL345 = 0x53; // The ADXL345 sensor I2C address
-
-float X, Y, Z;  // Outputs
-
-double init_acc;
-double MAX_VALUE = 2.0;
-double MIN_VALUE = 0.0;
-double RANGE = 0.1;
-
-
-
 RGBLed led(RED_RGB, GREEN_RGB, BLUE_RGB);
 Buzzer buzzer(BUZZER);
-
-LDR ldr(LDR0, LDR1, LDR2, 1000);
+LDR ldr(LDR0, LDR1, LDR2, LDR_SENSITIVITY);
 Lid lid(LID, 200);
-// Accelerometer accelerometer;
 
-RF24 radio(CE, CSN); // CE, CSN
-const byte address[6] = "00001"; // the address the the module
+int ADXL345 = 0x53; 
+float X, Y, Z;  
+double init_acc;
 
 bool isUnlocked = false;
 bool isOpened = false;
 bool isAlarmOff = false;
-String alarm = "";
 int boardID = 1;
+String alarmType;
+int alarmTime = 20;
+
+RF24 radio(CE, CSN);
+const byte address[6] = "00001"; 
 
 MFRC522 rfid(SS_PIN, RST_PIN);
 byte ID[4] = {154, 248, 194, 21};
 
+
 void setup() {
   Serial.begin(9600);
   SPI.begin();
-  rfid.PCD_Init();
   
-  Wire.begin(); // Initiate the Wire library
-  // Set ADXL345 in measuring mode
-  Wire.beginTransmission(ADXL345); // Start communicating with the device 
-  Wire.write(0x2D); // Access/ talk to POWER_CTL Register - 0x2D
-  // Enable measurement
-  Wire.write(8); // (8dec -> 0000 1000 binary) Bit D3 High for measuring enable 
+  rfid.PCD_Init();
+  Wire.begin();
+  Wire.beginTransmission(ADXL345);
+  Wire.write(0x2D);
+  Wire.write(8);
   Wire.endTransmission();
   delay(10);
 
-  init_acc = readAccelerometer();
-  //Serial.println(init_acc);
-  
+  // buzzer.openningMusic();
+
   led.ledColor(255, 255, 255);
+  while(!isUnlocked)
+    readNfc();
+
+  init_acc = readAccelerometer();
 }
 
-void loop() {  
-  isMoved(); 
-  // Read NFC card
-  // Serial.println(accelerometer.readAccelerometer()[1]);
+
+void loop() {
+  readNfc();
+  
+  if(isAlarmOff) 
+  {
+    alarm();
+  }
+  else
+  {
+    if(!isUnlocked)
+    {
+      if(isMoved())
+      {
+        alarmTurnedOff("ACCELEROMETER");
+      }
+      else if(ldr.isTriggered())
+      {
+        alarmTurnedOff("LDR");
+      }
+      else if(lid.isTriggered())
+      {
+        alarmTurnedOff("LID");
+      }
+    }
+    else
+    {
+      if(lid.isTriggered())
+      {
+        Serial.println("triggered");
+        isOpened = true;
+        led.ledColor(0, 120, 255);
+        delay(125);
+        led.ledColor(0, 0, 0);
+        delay(100);
+      }
+      else if(!lid.isTriggered() && isOpened)
+      {
+        buzzer.lockSound();
+        led.ledColor(0, 0, 0);
+        isUnlocked = false;
+        isOpened = false;
+        init_acc = readAccelerometer();
+      }
+    }
+  }
+}
+
+
+void readNfc() {
   if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) {
     if (rfid.uid.uidByte[0] == ID[0] && rfid.uid.uidByte[1] == ID[1] && rfid.uid.uidByte[2] == ID[2] && rfid.uid.uidByte[3] == ID[3])
     {
       led.ledColor(0, 255, 0);
       isUnlocked = true;
-      buzzer.unlockSound(); 
-      led.ledColor(0, 255, 0);
+      buzzer.unlockSound();
+      delay(300);
       isAlarmOff = false;
       Serial.println("unlocked");
     } 
@@ -93,56 +132,94 @@ void loop() {
       isUnlocked = false;
     }
   }
+  rfid.PICC_HaltA();
+}
 
+
+bool alarmTurnedOff(String alarm) {
+  isAlarmOff = true;
+  alarmType = alarm;
+}
+
+
+void alarm() {
+  led.ledColor(255, 0, 0);
+  buzzer.alarmSound(0.25);
+  led.ledColor(0, 0, 0);
+  buzzer.alarmSound(0.25);
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.stopListening();
+  String text = getTransmitData();
+  Serial.println(text);
+  radio.write(&text, sizeof(text));
   
-  if(isAlarmOff) 
+  if(alarmTime == 0)
   {
-    alarmTurnedOff();
+    init_acc = readAccelerometer();
+    alarmTime = 20;
+  }
+  alarmTime = alarmTime - 1;
+}
+
+
+String getTransmitData() {
+  readAccelerometer();
+  ldr.isTriggered();
+
+  String isOpen;
+  String hasLight;
+  String isMoving;
+  
+  if(lid.isTriggered()) 
+  {
+    isOpen = "true";
   }
   else
   {
-    if(isMoved())
-    {
-      if(!isUnlocked) {
-        isAlarmOff = true;
-        alarm = "ACCELEROMETER";
-      }
-      Serial.println("moved");
-    }
-    else if(ldr.isTriggered())
-    {
-      if(!isUnlocked)
-      {
-        isAlarmOff = true;
-        alarm = "LDR";
-      }
-      Serial.println("light");
-    }
-    else if(lid.isTriggered())
-    {
-      if(isUnlocked)
-      {
-        isOpened = true;
-      }
-      else
-      {
-        isAlarmOff = true;
-        alarm = "LID";
-      }
-      Serial.println("lid opened");
-    }
-    else // Lock
-    {
-      if(isUnlocked && isOpened) 
-      {
-        isUnlocked = false;
-        isOpened == false;
-      }
-    }
+    isOpen = "false";
   }
-  // RFID
-  rfid.PICC_HaltA();
+
+  if(ldr.isTriggered()) 
+  {
+    hasLight = "true";
+  }
+  else
+  {
+    hasLight = "false";
+  }
+
+  if(isMoved()) 
+  {
+    isMoving = "true";
+  }
+  else
+  {
+    isMoving = "false";
+  }
+  
+  String json = "{\n\t\"device_id\": " + String(boardID) + ",\n\t\"alarm_type\": " + "\"" + alarmType + "\",\n\t\"is_open\": " + isOpen + ",\n\t\"is_moving\": " + isMoving + ",\n\t\"has_light\": " + hasLight + ",\n\t\"sensor\": {\n\t\t\"ldr\": [" + String(ldr.ldr0_value) + ", " + String(ldr.ldr1_value) + ", " + String(ldr.ldr2_value) + "],\n\t\t\"accelerometer\": [" + String(X) + ", " + String(Y) + ", " + String(Z) + "]\n\t}\n}";
+  
+  return json;
+  
 }
+
+
+bool isMoved()
+{
+    double acc = readAccelerometer();
+    double threshold = 0.05;
+    
+    if(acc > init_acc + threshold)
+    {
+      // Serial.println("true");
+      return true;
+    }
+    return false;
+
+}
+
 
 double readAccelerometer()
 {
@@ -165,57 +242,4 @@ double readAccelerometer()
     Wire.endTransmission();
 
     return acc;
-}
-
-bool isMoved()
-{
-    double acc = readAccelerometer();
-    double threshold = 0.05;
-    
-    if(acc > init_acc + threshold)
-    {
-      return true;
-    }
-    return false;
-}
-
-void alarmTurnedOff() {
-  led.ledColor(255, 0, 0);
-  buzzer.alarmSound(1);
-  radio.begin();
-  radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MAX);
-  radio.stopListening();
-  String text = getTransmitData(); // you can customize this text to your wish
-  radio.write(&text, sizeof(text));
-}
-
-/*
- * {
- *    "id": 1,
- *    "alarm": "LID",
- *    "sensor": {
- *      "lid": false,
- *      "ldr": [400, 350, 380],
- *      "accelerometer": [-0.08, 0.12, 0.02]
- *    }
- *  }
- */
-String getTransmitData() {
-  readAccelerometer();
-  ldr.isTriggered();
-
-  String lid_str;
-  
-  if(lid.isTriggered()) 
-  {
-    lid_str = "true";
-  }
-  else
-  {
-    lid_str = "false";
-  }
-
-  return "{ \"id\":" + String(boardID) + ", \"alar\": " + alarm + ", \"sensor\": { \"lid\": " + lid_str + ", \"ldr\": [" + String(ldr.ldr0_value) + ", " + String(ldr.ldr1_value) + ", " + String(ldr.ldr2_value) + "], \"accelerometer\": [" + String(X) + ", " + String(Y) + ", " + String(Z) + "] } }";
-  
 }
--- libraries/Buzzer/Buzzer.cpp
+++ libraries/Buzzer/Buzzer.cpp
@@ -1,15 +1,18 @@
 #include "Buzzer.h"
 
-Buzzer::Buzzer(byte BUZZER) {
-    this->BUZZER = BUZZER;
-    init();
+Buzzer::Buzzer(byte BUZZER)
+{
+  this->BUZZER = BUZZER;
+  init();
 }
 
-void Buzzer::init() {
-    pinMode(BUZZER, OUTPUT);
+void Buzzer::init()
+{
+  pinMode(BUZZER, OUTPUT);
 }
 
-void Buzzer::unlockSound() {
+void Buzzer::unlockSound()
+{
   tone(BUZZER, NOTE_E7);
   delay(120);
   tone(BUZZER, NOTE_G7);
@@ -19,7 +22,8 @@ void Buzzer::unlockSound() {
   noTone(BUZZER);
 }
 
-void Buzzer::lockSound() {
+void Buzzer::lockSound()
+{
   tone(BUZZER, NOTE_C8);
   delay(150);
   noTone(BUZZER);
@@ -30,13 +34,78 @@ void Buzzer::lockSound() {
 }
 
 // Play alarm sound by given duration. Duration must be seconds.
-void Buzzer::alarmSound(int duration) {
+void Buzzer::alarmSound(double duration)
+{
   // Every one loop takes aproximately 100 milliseconds.
-  for(int i = 0; i < duration * 10; i++) {
+  for (int i = 0; i < duration * 10; i++)
+  {
     tone(BUZZER, NOTE_E5);
     delay(40);
     tone(BUZZER, NOTE_C8);
     delay(60);
   }
   noTone(BUZZER);
+}
+
+void Buzzer::openningMusic()
+{
+  int tempo = 220;
+
+  int buzzer = 11;
+
+  int melody[] = {
+
+      NOTE_E5,
+      8,
+      NOTE_D5,
+      8,
+      NOTE_FS4,
+      4,
+      NOTE_GS4,
+      4,
+      NOTE_CS5,
+      8,
+      NOTE_B4,
+      8,
+      NOTE_D4,
+      4,
+      NOTE_E4,
+      4,
+      NOTE_B4,
+      8,
+      NOTE_A4,
+      8,
+      NOTE_CS4,
+      4,
+      NOTE_E4,
+      4,
+      NOTE_A4,
+      2,
+  };
+
+  int notes = sizeof(melody) / sizeof(melody[0]) / 2;
+
+  int wholenote = (60000 * 4) / tempo;
+
+  int divider = 0, noteDuration = 0;
+
+  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2)
+  {
+    divider = melody[thisNote + 1];
+    if (divider > 0)
+    {
+      noteDuration = (wholenote) / divider;
+    }
+    else if (divider < 0)
+    {
+      noteDuration = (wholenote) / abs(divider);
+      noteDuration *= 1.5;
+    }
+
+    tone(BUZZER, melody[thisNote], noteDuration * 0.9);
+
+    delay(noteDuration);
+
+    noTone(BUZZER);
+  }
 }
